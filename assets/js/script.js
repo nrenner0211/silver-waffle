e58ac4d1555837a65fd9d280f680d33b0922df66
nrenner <nrenner0211@gmail.com>
2022-06-06 03:15:23 -0400

    header

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var getWeather = function(location) {
+    //format api url
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=840&limit=5&appid=56983c8d4b843493cded7cc811106a23";
+
+}
+
