5bf5a46c4c4fa973af930f78b36ebb8013372363
nrenner <nrenner0211@gmail.com>
2022-06-06 23:52:04 -0400

    api connected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
-const emojiBtn = document.getElementById("emoji-btn");
+function weatherBalloon( cityID ) {
+    var key = '56983c8d4b843493cded7cc811106a23';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherBalloon( 6167865 );
+  }
+
+//header emoji content
+const iconArray = document.getElementById("iconArray");
 const stormY = ["🌩️", "⛈️", "☀️", "🌨️", "🌧️", "☁️", "🌤️", "🌦️"];
 
-
-
-
-
-emojiBtn.addEventListener("mouseover", () => {
-    emojiBtn.innerText = stormY[Math.floor(Math.random() * stormY.length)]
+iconArray.addEventListener("mouseover", () => {
+    iconArray.innerText = stormY[Math.floor(Math.random() * stormY.length)]
 });
 
 
